54f71c918faceb09c3237fc36052987eb3aac566
AngelikaGlaser <angelikaglaser2001@gmail.com>
2023-01-19 15:26:27 +0200

    Add data Json file and display it dynamically

--- index.js
+++ index.js
@@ -7,10 +7,25 @@ app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 //static files css js images
-
 app.use(express.static(path.join(__dirname, 'public')))
+
+
+// use file system - read and write files
+const fs = require('fs')
+
 app.get('/', (req, res) => {
-    res.render('index')
+    fs.readFile('./tasks.json', 'utf-8', (err, jsonString) => {
+        if(err){
+            console.log('Error reading file: ', err)
+            return
+        }
+        try {
+            const tasks = JSON.parse(jsonString)
+            res.render('index', {tasksData : tasks})
+        } catch (err) {
+            console.log('Error parsing JSON file: ', err)
+        }
+    })
 })
 
 app.listen(3002)
